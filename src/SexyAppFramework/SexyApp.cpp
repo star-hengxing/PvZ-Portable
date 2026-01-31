ac835f288d39d5796427a8ea52d8ce013aa52f9d
Zhou Qiankang <wszqkzqk@qq.com>
2026-01-31 10:54:03 +0800

    Switch to mordern C++'s string conversion
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- src/SexyAppFramework/SexyApp.cpp
+++ src/SexyAppFramework/SexyApp.cpp
@@ -534,15 +534,12 @@ void SexyApp::HandleCmdLineParam(const std::string& theParamName, const std::str
 
 std::string SexyApp::GetGameSEHInfo()
 {
-	char aGamesPlayedStr[16];
-	sprintf(aGamesPlayedStr, "%d", mTimesPlayed);
-
 	std::string anInfoString = SexyAppBase::GetGameSEHInfo() + 
-		"Times Played: " + std::string(aGamesPlayedStr) + "\r\n" +
+		"Times Played: " + std::to_string(mTimesPlayed) + "\r\n" +
 		"Build Num: " + StrFormat("%d", mBuildNum) + "\r\n" +
 		"Build Date: " + mBuildDate + "\r\n";
 
-	if (mReferId.length() != 0)
+	if (!mReferId.empty())
 	{
 		anInfoString +=
 			"ReferId: " + mReferId + "\r\n";
